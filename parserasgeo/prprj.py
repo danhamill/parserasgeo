34821885b384668db4fee183aed9e1793009f33c
Daniel Hamill <hamill.daniel@gmail.com>
2020-05-22 19:45:02 -0400

    added helper class to add data to project file

--- parserasgeo/prprj.py
+++ parserasgeo/prprj.py
@@ -21,10 +21,10 @@ class ProjectTitle(object):
         self.value = line.split('=')[1][:-1]
 
 
-        return next(prj_file)
+        return prj_file
 
     def __str__(self):
-        return 'Proj Title=' + str(self.value) + '\n'
+        return 'Proj Title=' + str(self.value)+ '\n'
 
 class PlanFiles(object):
     def __init__(self):
@@ -37,21 +37,23 @@ class PlanFiles(object):
         return False
 
     def import_prj(self, line, prj_file):
-        value = line.split('=')[1][:-1]
+        value = line.split('=')[1].strip()
         if value not in self.values:
             self.values.append(value)
 
-        return next(prj_file)
-
+        return prj_file
+    def add_plan(self, plan_str):
+        self.values.append(plan_str)
     def __str__(self):
         s = ''
         for value in self.values:
-            s += 'Plan File=' + str(value) + '\n'
+            s += 'Plan File=' + str(value)+ '\n'
         return s
 
 class GeomFiles(object):
     def __init__(self):
         self.values = []
+
 
     @staticmethod
     def test(line):
@@ -60,67 +62,107 @@ class GeomFiles(object):
         return False
 
     def import_prj(self, line, prj_file):
-        value = line.split('=')[1][:-1]
-        if value not in self.values:
-            self.values.append(value)
 
-        return next(prj_file)
+        value = line.split('=')[1].strip()
+        self.values.append(value)
+        return prj_file
+
+    def add_geom(self, geom_str):
+        self.values.append(geom_str)
 
     def __str__(self):
         s = ''
         for value in self.values:
-            s += 'Geom File=' + str(value) + '\n'
+            s += 'Geom File=' + str(value) +'\n'
         return s
+
+
+class UnknownParam(object):
+    def __init__(self):
+        self.value = None
+
+    def import_prj(self, line, prj_file):
+        self.value = line
+        return prj_file
+
+    def __str__(self):
+        return str(self.value) + + '\n'
 
 
 
 class ParseRASProject(object):
     def __init__(self, project_file):
+
         self.project_title = ProjectTitle()
         self.plan_files = PlanFiles()
         self.geom_files = GeomFiles()
-
-
-        self.parts = [self.project_title, self.plan_files, self.geom_files]
-
         self.prj_list = []
 
         with open(project_file, 'rt') as prj_file:
                 for line in prj_file:
+                    if ProjectTitle.test(line):
+                        pt = ProjectTitle()
+                        pt.import_prj(line, prj_file)
+                        self.prj_list.append(pt)
+                    elif GeomFiles.test(line):
+                        gf = GeomFiles()
+                        gf.import_prj(line, prj_file)
+                        self.prj_list.append(gf)
+                    elif PlanFiles.test(line):
+                        pf = PlanFiles()
+                        pf.import_prj(line, prj_file)
+                        self.prj_list.append(pf)
+                    else:
+                        uk = UnknownParam()
+                        uk.import_prj(line, prj_file)
+                        self.prj_list.append(line)
 
-                    while line != None:
-                        for part in self.parts:
-                            if part.test(line):
-                                # print str(type(part))+' found!'
-                                line = part.import_prj(line, prj_file)
-                                #self.parts.remove(part)
-                                self.prj_list.append(part)
-                                break
-                        else:  # Unknown line, add as text
-                            self.prj_list.append(line)
-                            line = next(prj_file,None)
-                    #return line
-
-    def __str__(self):
-        s = ''
-        for line in self.prj_list:
-            s += str(line)
-        return s + '\n'
 
     def write(self, out_prj_filename):
         with open(out_prj_filename, 'wt', newline='\r\n') as outfile:
             for line in self.prj_list:
                 outfile.write(str(line))
+
+class AddData(object):
+    def __init__(self, prj_list):
+        self.prj_list = prj_list
+
+    def add_geom_to_prj(self, geom_str):
+        #find index of last valid geom
+        g = [isinstance(e, GeomFiles) for e in self.prj_list]
+        idx_insert = [i for i, val in enumerate(g) if val][-1]
+        gf = GeomFiles()
+        gf.add_geom(geom_str)
+        self.prj_list.insert(idx_insert+1, gf)
+
+    def add_plan_to_prj(self, plan_str):
+
+        #find index of last valid geom
+        g = [isinstance(e, PlanFiles) for e in self.prj_list]
+        idx_insert = [i for i, val in enumerate(g) if val][-1]
+        pf = PlanFiles()
+        pf.add_plan(plan_str)
+        self.prj_list.insert(idx_insert+1, pf)
+
+    def write(self, out_prj_filename):
+        with open(out_prj_filename, 'wt', newline='\r\n') as outfile:
+            for line in self.prj_list:
+                outfile.write(line.__str__())
+
 def main():
 
-    prj = ParseRASProject('C:/Users/u4rrecse/Documents/RAS_FRAZIL/RAS Model/BaldEagleCrRAZFR.prj')
+    prj = ParseRASProject(r"C:\Users\RDCRLDDH\Desktop\NENMRHB4\NENMRHB4_hec_ras.prj")
 
-    print(str(prj))
-#    import sys
-#
-#    prp = ParseRASProject(sys.argv[1])
-#    print(dir(prp))
-#    print(str(prp))
+
+    new = AddData(prj.prj_list)
+
+    new.add_geom_to_prj('g09')
+    new.add_plan_to_prj('p09')
+
+
+    new.write(r"C:\Users\RDCRLDDH\Desktop\NENMRHB4\NENMRHB4_hec_ras.prj1")
+
+
 
 
 if __name__ == '__main__':
